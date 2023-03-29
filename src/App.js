2d41127e24e083ed8abe0b033043f7b2d2ecf0df
Cesar Cortes <cesarcortes4@outlook.com>
2023-03-29 14:31:23 -0500

    second commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React  from "react";
+import "./App.css"
+import Header from "./components/header/Header";
+import { Home } from "./components/home/Home";
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className="main">
+        <Home/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/Social.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/cesar-mateo-cortes-leon-b823a2206/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin">
+            
+        </i>
+      </a>
+
+      <a
+        href="https://github.com/cesarcortes-dep"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
