6d8357012cdf406fd00a0606b35593921cd9e527
Cesar Cortes <cesarcortes4@outlook.com>
2025-09-23 16:33:50 -0500

    Initial commit: React TypeScript Portfolio

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header/Header';
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
